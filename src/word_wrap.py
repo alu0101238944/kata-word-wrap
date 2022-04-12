8cd30e36871ac962b47efe4c51b9e5e11914f02a
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-12 18:58:51 +0100

    Initial algorithm

--- src/word_wrap.py
+++ src/word_wrap.py
@@ -1,4 +1,13 @@
 
 class WordWrap:
   def apply(self, expression, n_columns):
-    return '\n'.join(expression.split(' '))
+    if n_columns > len(expression):
+      return expression
+    parsedString = list(expression[:n_columns])
+    i = n_columns - 1
+    while i > 0:
+      if parsedString[i] == ' ':
+        parsedString[i] = '\n'
+        return ''.join(parsedString) + self.apply(expression[n_columns:], n_columns)
+      i -= 1
+    return ''.join(parsedString) + '\n' + self.apply(expression[n_columns:], n_columns)
--- word_wrap_test.py
+++ word_wrap_test.py
@@ -9,8 +9,12 @@ class WordWrapTests(unittest.TestCase):
   def test_empty_string_return_itself(self):
     self.assertEqual(self.wordWrap.apply('', 2), '')
 
-  def test_ncolumns_matches_with_space_then_replace_it_by_newline(self):
+  def test_ncolumns_matches_with_space_then_replace_it_with_newline(self):
     self.assertEqual(self.wordWrap.apply('a b', 2), 'a\nb')
+    self.assertEqual(self.wordWrap.apply('aa b', 3), 'aa\nb')
+
+  def test_some_words_in_each_line(self):
+    self.assertEqual(self.wordWrap.apply('aaa b b cc cc', 4), 'aaa\nb b\ncc\ncc')
 
 if __name__ == '__main__':
   unittest.main()
